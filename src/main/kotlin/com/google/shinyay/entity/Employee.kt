64f1213cfe8507b83da35f333f3188e14fd5b611
shinyay <shinya.com@gmail.com>
2020-12-15 10:16:11 +0900

    Add Entity for Employee

---
+++ src/main/kotlin/com/google/shinyay/entity/Employee.kt
@@ -0,0 +1,6 @@
+package com.google.shinyay.entity
+
+data class Employee(val id: Long,
+                    val name: String,
+                    val role: String,
+                    val department_id: Long)
