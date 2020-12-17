4947846b2b9632b1c3a061ee02b28ac005f76338
shinyay <shinya.com@gmail.com>
2020-12-17 23:08:19 +0900

    Add repository as Constructor Dependency Injection

--- src/main/kotlin/com/google/shinyay/controller/EmployeeController.kt
+++ src/main/kotlin/com/google/shinyay/controller/EmployeeController.kt
@@ -1,10 +1,11 @@
 package com.google.shinyay.controller;
 
+import com.google.shinyay.repository.EmployeeRepository
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class EmployeeController {
+class EmployeeController(val repository: EmployeeRepository) {
 
     @GetMapping("/employees")
     fun findAllEmployees() {}
