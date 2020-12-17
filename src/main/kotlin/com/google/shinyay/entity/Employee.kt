3c141bd72d0d7f942920e3039077f1dc0aa97e57
shinyay <shinya.com@gmail.com>
2020-12-17 23:25:06 +0900

    Add Table annotation to specify table name

--- src/main/kotlin/com/google/shinyay/entity/Employee.kt
+++ src/main/kotlin/com/google/shinyay/entity/Employee.kt
@@ -1,7 +1,9 @@
 package com.google.shinyay.entity
 
+import com.google.cloud.spring.data.spanner.core.mapping.Table
 import org.springframework.data.annotation.Id
 
+@Table(name = "employee")
 data class Employee(@Id val id: Long,
                     val name: String,
                     val role: String,
