ef26c6f811719cc8e3ed9d3f4023aee1fcf2a35a
shinyay <shinya.com@gmail.com>
2020-12-17 23:09:24 +0900

    Add call to repository function from controller

--- src/main/kotlin/com/google/shinyay/controller/EmployeeController.kt
+++ src/main/kotlin/com/google/shinyay/controller/EmployeeController.kt
@@ -8,5 +8,5 @@ import org.springframework.web.bind.annotation.RestController
 class EmployeeController(val repository: EmployeeRepository) {
 
     @GetMapping("/employees")
-    fun findAllEmployees() {}
+    fun findAllEmployees() = repository.getEmployees()
 }
