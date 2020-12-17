3bf6e12a7bc19ca54ae324a11f6633ef24f5fc18
shinyay <shinya.com@gmail.com>
2020-12-17 23:04:47 +0900

    Add Get API to fin all employees

--- src/main/kotlin/com/google/shinyay/controller/EmployeeController.kt
+++ src/main/kotlin/com/google/shinyay/controller/EmployeeController.kt
@@ -1,7 +1,11 @@
 package com.google.shinyay.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
-public class EmployeeController {
+class EmployeeController {
+
+    @GetMapping("/employees")
+    fun findAllEmployees() {}
 }
