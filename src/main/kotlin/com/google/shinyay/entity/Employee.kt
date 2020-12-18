436d8d1b342854583d26a5b380ce59a3515a1014
shinyay <shinya.com@gmail.com>
2020-12-18 10:32:45 +0900

    Add PrimaryKey Annotation for Spanner

--- src/main/kotlin/com/google/shinyay/entity/Employee.kt
+++ src/main/kotlin/com/google/shinyay/entity/Employee.kt
@@ -1,10 +1,11 @@
 package com.google.shinyay.entity
 
+import com.google.cloud.spring.data.spanner.core.mapping.PrimaryKey
 import com.google.cloud.spring.data.spanner.core.mapping.Table
-import org.springframework.data.annotation.Id
 
 @Table(name = "employee")
-data class Employee(@Id val id: Long,
+data class Employee(@PrimaryKey(keyOrder = 1)
+                    val id: Long,
                     val name: String,
                     val role: String,
                     val department_id: Long)
