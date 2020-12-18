c0a344cc0e46fe30793cac1ce3168c4dc02a0a83
shinyay <shinya.com@gmail.com>
2020-12-18 10:33:17 +0900

    Add Columname specification for Spanner

--- src/main/kotlin/com/google/shinyay/entity/Employee.kt
+++ src/main/kotlin/com/google/shinyay/entity/Employee.kt
@@ -1,11 +1,14 @@
 package com.google.shinyay.entity
 
+import com.google.cloud.spring.data.spanner.core.mapping.Column
 import com.google.cloud.spring.data.spanner.core.mapping.PrimaryKey
 import com.google.cloud.spring.data.spanner.core.mapping.Table
 
 @Table(name = "employee")
 data class Employee(@PrimaryKey(keyOrder = 1)
+                    @Column(name="employee_id")
                     val id: Long,
+                    @Column(name = "employee_name")
                     val name: String,
                     val role: String,
                     val department_id: Long)
