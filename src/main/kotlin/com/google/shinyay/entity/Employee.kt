f4768c487936d304bde49b9ea6df05b61c76cc61
shinyay <shinya.com@gmail.com>
2020-12-22 23:36:47 +0900

    Modify val to var

--- src/main/kotlin/com/google/shinyay/entity/Employee.kt
+++ src/main/kotlin/com/google/shinyay/entity/Employee.kt
@@ -7,8 +7,8 @@ import com.google.cloud.spring.data.spanner.core.mapping.Table
 @Table(name = "employee")
 data class Employee(@PrimaryKey(keyOrder = 1)
                     @Column(name="employee_id")
-                    val id: Long,
+                    var id: Long,
                     @Column(name = "employee_name")
-                    val name: String?,
-                    val role: String?,
+                    var name: String?,
+                    var role: String?,
                     val department_id: Long?)
