e03fa92943a013ff26331a0049ea627cf127b890
shinyay <shinya.com@gmail.com>
2020-12-24 18:33:41 +0900

    Add find all Employees with SpannerTemplate

--- src/main/kotlin/com/google/shinyay/repository/EmployeeCustomRepository.kt
+++ src/main/kotlin/com/google/shinyay/repository/EmployeeCustomRepository.kt
@@ -1,7 +1,17 @@
 package com.google.shinyay.repository
 
-import org.springframework.stereotype.Repository
+import com.google.cloud.Timestamp
+import com.google.cloud.spanner.Statement
+import com.google.cloud.spring.data.spanner.core.SpannerQueryOptions
+import com.google.cloud.spring.data.spanner.core.SpannerTemplate
+import com.google.shinyay.entity.Employee
+import org.springframework.stereotype.Component
 
-@Repository
-class EmployeeCustomRepository {
+@Component
+class EmployeeCustomRepository(val spannerTemplate: SpannerTemplate) {
+
+    fun findEmployeeAll(): MutableList<Employee>? {
+        val spannerQueryOptions = SpannerQueryOptions().setTimestamp(Timestamp.now())
+        return spannerTemplate.query(Employee::class.java, Statement.of("SELECT * FROM employee"), spannerQueryOptions)
+    }
 }
